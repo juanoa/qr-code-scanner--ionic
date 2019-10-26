817f8b36e58d7799a94bdf96494967e99a2c8f25
Juan <juan@juanoa.com>
2019-10-26 12:15:27 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: "",
+    loadChildren: () =>
+      import("./pages/tabs/tabs.module").then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
